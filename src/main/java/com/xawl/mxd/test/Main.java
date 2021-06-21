8fc3ce6803ae71355c1fc447b02cf7ab1fd2a7e7
mxd-321 <m18220883121@163.com>
2021-06-21 21:30:28 +0800

    测试父类和子类谁先初始化

--- src/main/java/com/xawl/mxd/test/Main.java
+++ src/main/java/com/xawl/mxd/test/Main.java
@@ -1,35 +1,107 @@
 package com.xawl.mxd.test;
 
+import org.junit.Test;
+
 /**
  * @Author mxd
  * @Date 2021/5/27 16:38
+ *  结论：初始化子类时，若父类没有初始化，则先初始化父类、
+ *      父类优先、静态先行、非静态比构造优先
  */
 
 public class Main {
-    static Object object1= new Object();
-    static Object object2 = new Object();
 
-    public static void main(String[] args) {
-        new Thread(()->{
-            synchronized (object1){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                synchronized (object2){
+    @Test
+    public void method1() {
+        /**
+         * parent static
+         * son static
+         * parent no static
+         * parent 构造
+         * son no static
+         * son 构造
+         */
+        new Son();
+    }
 
-                }
-            }
-        }).start();
+    /**
+     * parent static
+     * son static
+     * parent no static
+     * parent 构造
+     * son no static
+     * son 构造
+     *
+     * parent no static
+     * parent 构造
+     * son no static
+     * son 构造
+     */
+    @Test
+    public void method2() {
+        new Son();
+        System.out.println();
+        new Son();
+    }
 
-        new Thread(()->{
-            synchronized (object2){
-                synchronized (object1){
+    /**
+     * parent static
+     * son static
+     * parent no static
+     * parent 构造
+     * son no static
+     * son 构造
+     *
+     * parent no static
+     * parent 构造
+     */
+    @Test
+    public void method3() {
+        new Son();
+        System.out.println();
+        new Parent();
+    }
 
-                }
-            }
-        }).start();
+    /**
+     * parent static
+     * parent no static
+     * parent 构造
+     */
+    @Test
+    public void method4() {
+        new Parent();
+    }
 
+    /**
+     * parent static
+     * parent no static
+     * parent 构造
+     *
+     * son static
+     * parent no static
+     * parent 构造
+     * son no static
+     * son 构造
+     */
+    @Test
+    public void method5() {
+        new Parent();
+        System.out.println();
+        new Son();
+    }
+
+    /**
+     * parent static
+     * parent no static
+     * parent 构造
+     *
+     * parent no static
+     * parent 构造
+     */
+    @Test
+    public void method6() {
+        new Parent();
+        System.out.println();
+        new Parent();
     }
 }
